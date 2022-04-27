841047dbecd835b362a87437e318cc0b8bbf9e39
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-04-27 11:33:01 +0100

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Sidebar from './Sidebar';
+import Submenu from './Submenu';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h2>A Stripe Inspired App</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from './images/logo.svg';
+import { FaBars } from 'react-icons/fa';
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
